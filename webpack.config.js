e5200949640784d6c5fbf0b1060aaac06c125b68
Amine <hamdi.amine.dev@gmail.com>
2020-09-05 13:51:20 +0100

    update config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/react-rounded-image.js",
+  output: {
+    path: path.resolve("lib"),
+    filename: "react-rounded-image.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+};
